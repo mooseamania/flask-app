1d931772677378824b0522edf34485d7e5220fb5
Seamus Owens <seamus.owens@gmail.com>
2018-12-11 17:58:58 -0500

    Update flaskblog.py

--- flaskblog.py
+++ flaskblog.py
@@ -3,9 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Bay Ridge World!"
+    return "<h1>Hello Bay Ridge World!</h1>"
 
-
+@app.route("/about")
+def hello():
+    return "<h2>This is about your mom</h2>"
 ## Test Language ##
 
 
