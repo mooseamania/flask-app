118bc7aabbea2f6e77c52cd58f8fa3797c6922a2
Seamus Owens <seamus.owens@gmail.com>
2018-12-12 16:25:16 -0500

    forms with bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+form forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '656e74657220796f7572207068726173652068657265'
+
 
 posts = [
 	{
@@ -28,6 +32,18 @@ def home():
 def about():
     return render_template('about.html', title='About')
 ## Test Language ##
+
+@app.route("/register")
+def register():
+	form = RegistrationForm()
+	return render_template('register.html', title='Register', form=form)
+
+	@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Login', form=form)
+
+
 
 if __name__ == '__main__':
 	app.run(
