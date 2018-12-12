13b537b3c11b37d5de9345f60bc49ff03489f753
Seamus Owens <seamus.owens@gmail.com>
2018-12-12 10:57:45 -0500

    posts now

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,32 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+	{
+		'author': 'Corey Shafer',
+		'title': 'Blog Post 1',
+		'content': 'first post content',
+		'date_posted': 'April 20, 2018'
+	},
+	{
+		'author': 'Jane Doe',
+		'title': 'Blog Post 2',
+		'content': 'second post content',
+		'date_posted': 'April 21, 2018'
+	}
+
+]
+
+
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About</h1>"
+    return render_template('about.html')
 ## Test Language ##
 
 if __name__ == '__main__':
