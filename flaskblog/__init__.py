d646eff53a8c6b97cd706fc3add6f2c9b1ced3bb
Seamus Owens <seamus.owens@gmail.com>
2018-12-31 13:01:18 -0500

    123

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
