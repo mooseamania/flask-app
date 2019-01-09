b28c01b105f7529cee5f70e4873621d9e398f778
Seamus Owens <seamus.owens@gmail.com>
2019-01-09 06:49:23 -0500

    Update routes.py

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -107,5 +107,5 @@ def new_post():
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
-    post = Post.query.get_or_404(post.id)
+    post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
