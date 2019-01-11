6949ff1d7a4055deb07ae9528f49527da404e1e7
Seamus Owens <seamus.owens@gmail.com>
2019-01-11 16:33:09 -0500

    Update routes.py

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -154,7 +154,7 @@ def user_posts(username):
 
 def send_reset_email(user):
     token = user.get_reset_token()
-    msg = Message('Password Reset Request', sender='noreply@demo.com', recipients=[user.email])
+    msg = Message('Password Reset Request', sender='seamus.owens@gmail.com', recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
 {url_for('reset_token', token=token, _external=True)}
 
