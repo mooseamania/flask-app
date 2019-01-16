0010efb356627d651814fce3053e6e3338058cfb
Seamus Owens <seamus.owens@gmail.com>
2019-01-16 13:41:18 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
-from flaskblog import create_app, db
+from flaskblog import create_app
 
-app = create.app()
+app = create_app()
 
 if __name__ == '__main__':
-	app.run(
-		debug=True,
-		host='0.0.0.0',
-		port='5000')
+    app.run(debug=True)
